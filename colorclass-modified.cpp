30bd0bd4138d4cd0bf70c4107220b3db694f8761
Chevin Jeon <109643560+Chevinjeon@users.noreply.github.com>
2025-09-29 02:48:39 -0700

    first commit

---
+++ colorclass-modified.cpp
@@ -0,0 +1,7 @@
+// COMSC-210 | Lab 16 | Chevin Jeon
+// IDE used: VSC
+// Date: 2025-09-29
+
+#include <iostream>   // std::cout, std::endl
+#include <iomanip>    // std::hex, std::setw, std::setfill, std::uppercase
+#include <stdexcept>  // std::out_of_range
